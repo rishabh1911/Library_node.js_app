be96daafe425e2874125670cd1c75541a8a0dae1
Rishabh Agarwal <rishabhagarwal1911@gmail.com>
2018-08-09 19:12:40 +0530

    added books page

--- app.js
+++ app.js
@@ -29,21 +29,74 @@ app.set('view engine', 'ejs');
 //   // of mac and windows so we use path
 //   response.sendFile(path.join(__dirname, 'views', 'index.html'));
 // });
+const books = [
+  {
+    title: 'War and Peace',
+    genre: 'Historical Fiction',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  },
+  {
+    title: 'Les Misérables',
+    genre: 'Historical Fiction',
+    author: 'Victor Hugo',
+    read: false
+  },
+  {
+    title: 'The Time Machine',
+    genre: 'Science Fiction',
+    author: 'H. G. Wells',
+    read: false
+  },
+  {
+    title: 'A Journey into the Center of the Earth',
+    genre: 'Science Fiction',
+    author: 'Jules Verne',
+    read: false
+  },
+  {
+    title: 'The Dark World',
+    genre: 'Fantasy',
+    author: 'Henry Kuttner',
+    read: false
+  },
+  {
+    title: 'The Wind in the Willows',
+    genre: 'Fantasy',
+    author: 'Kenneth Grahame',
+    read: false
+  },
+  {
+    title: 'Life On The Mississippi',
+    genre: 'History',
+    author: 'Mark Twain',
+    read: false
+  },
+  {
+    title: 'Childhood',
+    genre: 'Biography',
+    author: 'Lev Nikolayevich Tolstoy',
+    read: false
+  }];
+const nav = [{ title: 'Books', link: '/books' },
+  { title: 'Authors', link: 'authors' }];
 
 app.use('/books', bookRouter);
 
 bookRouter.route('/')
   .get((req, res) => {
-    res.send('Hello books');
+    res.render('books', {
+      title: 'books',
+      books,
+      nav
+    });
   });
 
 app.get('/', (request, response) => {
   response.render(
     'index',
     {
-      nav:
-        [{ title: 'Books', link: '/books' },
-          { title: 'Authors', link: 'authors' }],
+      nav,
       title: 'Library'
     }
   );
